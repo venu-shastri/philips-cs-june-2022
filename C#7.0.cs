b5aac0ed0274fe2f7ba281d1869d7807ca923117
Venugopal K <venu.shastri@gmail.com>
2022-05-26 18:32:18 +0530

    Create C#7.0.cs

---
+++ C#7.0.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Collections.Generic;
+				
+public class Environment{
+	
+	public string Name{get;set;}
+}
+public class Program
+{
+	
+	static Dictionary<string,Action> _depolyStatergies=new  Dictionary<string,Action>();
+	static Program(){
+		
+		_depolyStatergies.Add("Test",new Action(TestDeploy));
+		_depolyStatergies.Add("Production",new Action(ProductionDeploy));
+		_depolyStatergies.Add("QA",new Action(QADeploy));
+	}
+	public static void Main()
+	{
+		//out variables
+		Console.WriteLine("Hello World");
+		string data="1234";
+		//preDeclare variable
+		//int targetValue;
+		ConvertToInt(data,out int targetValue,out int code); // C# 7.0
+		ConvertToInt(data,out var _targetValue,out _); 
+		
+		object obj=GetObject();
+		if(obj is null){
+			Console.WriteLine("Obj is Null");
+		}
+		
+		else if(obj is Program){
+			Console.WriteLine("Obj is of Type Program");
+		}
+		else if(obj is int x){
+			//int x=(int)obj;
+			Console.WriteLine($"Obj is of Type Integer and value is {x}" );
+		}
+		
+		/* Is Expressions with Patterns - C# 7.0
+		if(obj is int i || obj is string s && 
+		*/
+		
+		Environment _env=new Environment(){ Name="Test"};
+		Deploy(_env);
+		_env.Name="Production";
+		Deploy(_env);
+		_env.Name="QA";
+		Deploy(_env);
+	}
+	
+	public static bool ConvertToInt(string input,out int convertedValue,out int code){
+		
+		convertedValue=0;
+		code=0;
+		return true;
+	}
+	
+	public static object GetObject(){
+		return 10;
+	}
+	
+	
+	
+	public static void Deploy(Environment env){
+		switch(env.Name){
+				
+			case string s when(s == "Test"):Console.WriteLine($"Test Env Deploy" );break;
+			
+		}
+		
+		//_depolyStatergies[env.Name].Invoke();
+		
+		
+	}
+	
+	public static void TestDeploy(){
+	Console.WriteLine($"Test Env Deploy" );
+	}
+	public static void ProductionDeploy(){
+	Console.WriteLine($"Production Env Deploy" );
+	}
+	public static void QADeploy(){
+	Console.WriteLine($"QA Env Deploy" );
+	}
+	
+}
